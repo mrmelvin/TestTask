10c4d688fa3d79172ab2e34d76965859b15d9fcb
mike <korobov.mike@gmail.com>
2023-09-07 17:36:56 +0500

    Add README.md and delete tmp file

--- src/test/java/support/Constants.java
+++ src/test/java/support/Constants.java
@@ -6,7 +6,6 @@ import java.util.Map;
 public final class Constants {
 	public final static String CONFIG_FILE = "credentials.json";
 	public final static String BASE_URL = "https://testslotegrator.com";
-	public final static Integer PLAYERS_NUMBER = 12;
 	public final static Map<String, String> HEADERS = Map.of("accept", "application/json", "Content-Type", "application/json");
 	public final static Map<String, String> CREDENTIALS = Map.of("email", "korobov.mike@gmail.com", "password", "f5QOQ2J0iJPF");
 
